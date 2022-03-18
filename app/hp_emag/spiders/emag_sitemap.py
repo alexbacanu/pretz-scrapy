6bb6edbd151644b445c0cb578eb8b817d2af1625
Alexandru Bacanu <alex.bacanu@live.com>
2022-03-18 17:34:10 +0200

    Update emag_sitemap.py

--- app/hp_emag/spiders/emag_sitemap.py
+++ app/hp_emag/spiders/emag_sitemap.py
@@ -1,10 +1,30 @@
-import scrapy
+from hp_emag.items import EmagSitemapItem
+from bs4 import BeautifulSoup
+from scrapy import Spider
 
 
-class EmagSitemapSpider(scrapy.Spider):
-    name = 'emag_sitemap'
-    allowed_domains = ['emag.ro']
-    start_urls = ['http://emag.ro/']
+class EmagSitemapSpider(Spider):
+    name = "emag_sitemap"
+    allowed_domains = ["emag.ro"]
+    # TODO: Get first index.xml and then 0.xml in case there are multiple files
+    start_urls = ["https://www.emag.ro/sitemaps/categories-0.xml"]
+
+    custom_settings = {
+        "SPIDER_MIDDLEWARES": {},
+        "DOWNLOADER_MIDDLEWARES": {},
+        "ITEM_PIPELINES": {
+            "hp_emag.pipelines.RedisPipelineSitemap": 250,
+        },
+    }
 
     def parse(self, response):
-        pass
+        # Find all objects in response text with <loc> tag
+        sp = BeautifulSoup(response.text, "lxml")
+        links = sp.find_all("loc")
+
+        for link in links:
+            # Declare items
+            item = EmagSitemapItem()
+            item["url"] = link.text
+
+            yield item
