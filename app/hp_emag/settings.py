79a138d1c5a1bcaa72eea628f667c2bb1fbb10ac
Alexandru Bacanu <alex.bacanu@live.com>
2022-03-18 18:17:54 +0200

    Enabled AutoThrottle

--- app/hp_emag/settings.py
+++ app/hp_emag/settings.py
@@ -13,13 +13,15 @@ USER_AGENT = "hp_emag/0.4 (Windows NT 10.0; Win64; x64)"
 ROBOTSTXT_OBEY = True
 
 #  Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 5
 
 #  Configure a delay for requests for the same website (default: 0)
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 #  The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
+#  Retries
+RETRY_TIMES = 3
 
 #  Disable cookies (enabled by default)
 COOKIES_ENABLED = False
@@ -58,9 +60,9 @@ ITEM_PIPELINES = {
 }
 
 #  Enable and configure the AutoThrottle extension (disabled by default)
-# AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 #  The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 #  The maximum download delay to be set in case of high latencies
 # AUTOTHROTTLE_MAX_DELAY = 60
 #  The average number of requests Scrapy should be sending in parallel to each remote server
