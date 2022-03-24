0c478bd3ee0a136b83d8115050738660386727fe
Alexandru Bacanu <alex.bacanu@live.com>
2022-03-24 02:52:57 +0200

    Added AmazonDynamoDBPipeline

--- app/hp_emag/pipelines.py
+++ app/hp_emag/pipelines.py
@@ -3,6 +3,7 @@
 
 import os
 
+import boto3
 import redis
 from redis.commands.json.path import Path
 
@@ -12,6 +13,54 @@ class DefaultValuesPipeline(object):
         # Make items have a default value (which is 0)
         for field in item.fields:
             item.setdefault(field, 0)
+
+        return item
+
+
+class AmazonDynamoDBPipeline(object):
+    def open_spider(self, spider):
+        # Init DB
+        self.dynamodb = boto3.resource("dynamodb", region_name="eu-central-1")
+        self.timeseries_table = self.dynamodb.Table("emag-timeseries")
+        self.products_table = self.dynamodb.Table("emag-products")
+        self.timeseries_items = []
+        self.products_items = []
+
+    def close_spider(self, spider):
+        # Write all items to emag-timeseries after spider is finished
+        with self.timeseries_table.batch_writer() as batch:
+            for i in range(len(self.timeseries_items)):
+                batch.put_item(self.timeseries_items[i])
+
+        # Write all items to emag-products after spider is finished
+        with self.products_table.batch_writer() as batch:
+            for i in range(len(self.products_items)):
+                batch.put_item(self.products_items[i])
+
+    def process_item(self, item, spider):
+        # Add values in a list for emag-timeseries
+        self.timeseries_items.append(
+            {
+                "id": item["id"],
+                "date": item["crawled"],
+                "rrp": item["rrp"],
+                "full": item["full"],
+                "price": item["price"],
+            }
+        )
+        # Add values in a list for emag-products
+        self.products_items.append(
+            {
+                "id": item["id"],
+                "name": item["name"],
+                "rrp": item["rrp"],
+                "full": item["full"],
+                "price": item["price"],
+                "link": item["link"],
+                "img": item["img"],
+                "crawled": item["crawled"],
+            }
+        )
 
         return item
 
@@ -56,8 +105,8 @@ class RedisPipelineProductsJSON(object):
         # Add scraped items to Redis using JSON (pipeline)
         product = {}
 
-        for k, v in item.items():
-            product[k] = v if v is not None else ""
+        for key, value in item.items():
+            product[key] = value if value is not None else ""
 
         self.pipe.json().set(f'json:{item["id"]}', Path.rootPath(), product)
 
