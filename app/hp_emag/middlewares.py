86d87c67b1002ec9793de2064263db6e8466a7d6
Alexandru Bacanu <alex.bacanu@live.com>
2022-03-28 20:15:11 +0300

    Updated retries, added exception handling WIP

--- app/hp_emag/middlewares.py
+++ app/hp_emag/middlewares.py
@@ -18,12 +18,17 @@ class AmazonStartUrlsMiddleware:
         self.dynamodb = boto3.resource(
             "dynamodb",
             region_name="eu-central-1",
-            config=Config(retries={"max_attempts": 15, "mode": "standard"}),
+            config=Config(retries={"max_attempts": 20, "mode": "adaptive"}),
         )
 
         self.start_urls_table = self.dynamodb.Table("emag-start_urls")
 
     def process_start_requests(self, start_requests, spider):
+        # Statuses:
+        # 0 - not crawled
+        # 1 - crawled
+        # response.status - error code
+
         # How many start_requests
         start_urls_pops = 4
 
@@ -34,12 +39,22 @@ class AmazonStartUrlsMiddleware:
         # Pop x entries from database and return their value
         for _ in range(start_urls_pops):
             get_url = self.start_urls_table.update_item(
-                Key={"status": 0},
+                Key={"status_code": 0},
                 ReturnValues="UPDATED_OLD",
                 UpdateExpression="REMOVE crawled_urls[0]",
             )["Attributes"]["crawled_urls"][0]
 
-            split_request = get_url.rsplit("/", 1)
-            request = f"{split_request[0]}/vendor/emag/c"
+            yield Request(url=get_url)
 
-            yield Request(url=request)
+    def process_spider_exception(self, response, exception, spider):
+        # XXX: Still testing
+        logger.exception("Exception in spider %s on %s: %s", spider.name, response.url, response.status)
+
+        # Add crawled_urls to database with status 2
+        self.start_urls_table.put_item(
+            Item={
+                "status_code": response.status,
+                "crawled_urls": [response.url],
+            },
+            ConditionExpression="attribute_not_exists(status_code) AND attribute_not_exists(crawled_urls)",
+        )
