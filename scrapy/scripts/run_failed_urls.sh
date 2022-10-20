9963d1736ec6e69bd7ec48692f98f796b4d578fa
Alexandru Bacanu <alex.bacanu@live.com>
2022-10-20 16:18:17 +0300

    cleanup

---
+++ scrapy/scripts/run_failed_urls.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. ~/.bash_profile
+
+# Move failed url to start_urls
+redis-cli -u $REDIS_URI SREM emag_products:failed_urls "https://www.emag.ro/robots.txt"
+redis-cli -u $REDIS_URI SUNIONSTORE emag_sitemap:start_urls emag_products:failed_urls emag_sitemap:start_urls
+redis-cli -u $REDIS_URI DEL emag_products:failed_urls
+
+# Clear temp files
+rm -r /tmp/emag_*
